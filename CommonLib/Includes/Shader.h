07b25f15e0785e9c14f3b6d179689326d7e7754d
Lin <linyuan5856139@163.com>
2018-03-06 14:52:43 +0800

    update shader.cpp && 6.corrdinatesystems

--- CommonLib/Includes/Shader.h
+++ CommonLib/Includes/Shader.h
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <glm/detail/type_vec2.hpp>
+#include <glm/mat2x3.hpp>
 
 
 using namespace std;
@@ -24,6 +26,25 @@ public:
 	void setBool(const string &name, bool value) const;
 	void setInt(const string &name, int value) const;
 	void setFloat(const string &name, float value) const;
+
+	void setVec2(const string &name, const glm::vec2 &value) const;
+	
+	void setVec2(const string &name, float x, float y) const;
+	
+	void setVec3(const string &name, const glm::vec3 &value) const;
+	
+	void setVec3(const string &name, float x, float y, float z) const;
+	
+	void setVec4(const string &name, const glm::vec4 &value) const;
+	
+	void setVec4(const string &name, float x, float y, float z, float w);
+	
+	void setMat2(const string &name, const glm::mat2 &mat) const;
+	
+	void setMat3(const string &name, const glm::mat3 &mat) const;
+	
+	void setMat4(const string &name, const glm::mat4 &mat) const;
+	
 
 private:
 	void checkCompileErrors(unsigned int, string);
